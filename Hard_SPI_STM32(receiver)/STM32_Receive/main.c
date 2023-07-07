0b46bdcd577ee17af913f3210323ebc76eff1150
Nguyễn Phi Hùng <82272158+hungneyugn@users.noreply.github.com>
2023-07-07 21:09:44 +0700

    Update main.c

--- Hard_SPI_STM32(receiver)/STM32_Receive/main.c
+++ Hard_SPI_STM32(receiver)/STM32_Receive/main.c
@@ -5,28 +5,29 @@
 #include "stdio.h"
 
 
-#define SPIx_RCC				RCC_APB2Periph_SPI1
-#define SPIx						SPI1
+#define SPIx_RCC		RCC_APB2Periph_SPI1
+#define SPIx			SPI1
 #define SPI_GPIO_RCC		RCC_APB2Periph_GPIOA
-#define SPI_GPIO				GPIOA
+#define SPI_GPIO		GPIOA
 #define SPI_PIN_MOSI		GPIO_Pin_7
 #define SPI_PIN_MISO		GPIO_Pin_6
-#define SPI_PIN_SCK			GPIO_Pin_5
-#define SPI_PIN_SS			GPIO_Pin_4
+#define SPI_PIN_SCK		GPIO_Pin_5
+#define SPI_PIN_SS		GPIO_Pin_4
 
 typedef SPI_InitTypeDef Spi_ConfigType;
 typedef uint8_t Std_ReturnType;
 typedef uint8_t Spi_ChannelType;
 typedef uint8_t Spi_DataBufferType;
 
-#define E_OK 					(Std_ReturnType)0
-#define E_NOT_OK 				(Std_ReturnType)1
+#define E_OK 			(Std_ReturnType)0
+#define E_NOT_OK 		(Std_ReturnType)1
 
 void Spi_Init (Spi_ConfigType* ConfigPtr);
 Std_ReturnType Spi_DeInit (void);
 Std_ReturnType Spi_WriteIB (Spi_ChannelType Channel,const Spi_DataBufferType* DataBufferPtr);
 Std_ReturnType Spi_ReadIB (Spi_ChannelType Channel,Spi_DataBufferType* DataBufferPointer);
 uint8_t receivedData;
+
 int main(void){
 	Spi_ConfigType ConfigPtr;
 	Spi_Init (&ConfigPtr);
